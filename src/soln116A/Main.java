cd5ed2f502729ad28aff79602b52e1170df2d51d
Devesh Shetty <devesh.shetty007@gmail.com>
2017-07-04 13:22:38 +0530

    Solve problem 116A (close #1)

---
+++ src/soln116A/Main.java
@@ -0,0 +1,97 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve( in , out);
+        out.close();
+    }
+
+    static class Task
+    {
+        public void solve(InputReader in , PrintWriter out)
+        {
+            int n = in.nextInt();
+            int minCapacity = 0;
+            int currentCount = 0;
+            for(int i = 0; i < n; i++)
+            {
+              int a = in.nextInt();
+              int b = in.nextInt();
+              currentCount -= a;
+              currentCount += b;
+              minCapacity = Math.max(minCapacity, currentCount);
+            }
+
+            out.print(minCapacity);
+        }
+    }
+
+    static class InputReader
+    {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream)
+        {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next()
+        {
+            while (tokenizer == null || !tokenizer.hasMoreTokens())
+            {
+                try
+                {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                }
+                catch (IOException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = reader.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        public long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        public int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+    }
+}
